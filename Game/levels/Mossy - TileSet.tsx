8c807a9581c186157c3c5d11a038a0456a0f187f
Salio24 <62927095+Salio24@users.noreply.github.com>
2025-04-03 21:00:10 +0300

    Delete Game directory

--- Game/levels/Mossy - TileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Mossy - TileSet" tilewidth="512" tileheight="512" tilecount="49" columns="7">
- <image source="cavern_assets/Mossy - TileSet.png" width="3584" height="3584"/>
-</tileset>
